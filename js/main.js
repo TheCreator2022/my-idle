82b22eb0fa34815a0d19b4f435e1c6427beda085
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-07-27 17:44:17 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 var dollars = new Decimal(0)
 var dps = new Decimal(0)
 var dpc = new Decimal(1)
+let costs = [new Decimal(30)]
 
 document.getElementById("click").onclick = () => {
   dollars = dollars.add(dpc)
 }
 
+document.getElementById("cost1").onclick = () => {
+  if (dollars.gte(costs[0])) {
+    dollars = dollars.sub(costs[0])
+    dps = dps.add(1)
+  }
+}
+
 function Update() {
  dollars = dollars.add(dps)
  document.getElementById("dollars").innerHTML = "You Have " + dollars + " Dollars."
+ document.getElementById("costNumber1").innerText = costs[0]
 }
 
 setInterval(Update, 100)
