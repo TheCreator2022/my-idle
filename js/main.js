029fc256495f6587f532d1836e24c337ef644734
TheCreator2022 <106817598+TheCreator2022@users.noreply.github.com>
2022-07-27 16:55:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 var dollars = new Decimal(0)
 var dps = new Decimal(0)
 var dpc = new Decimal(1)
+
+document.getElementById("click").onclick = () => {
+  dollars = dollars.add(dpc)
+}
+
+function Update() {
+ dollars = dollars.add(dps)
+ document.getElementById("dollars").innerHTML = "You Have " + dollars + " Dollars."
+}
+
+setInterval(Update, 100)
